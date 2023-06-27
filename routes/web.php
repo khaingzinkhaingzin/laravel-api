8688c738fadb63dc93383ea33e09c54ccfc1bacd
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2023-06-27 14:13:33 +0630

    Laravel api with sanctum

--- routes/web.php
+++ routes/web.php
@@ -19,25 +19,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/setup', function () {
-//     $credentials = [
-//         'email' => 'admin@admin.com',
-//         'password' => 'password',
-//     ];
+Route::get('/setup', function () {
+    $credentials = [
+        'email' => 'admin@admin.com',
+        'password' => 'password',
+    ];
 
-//     if (!Auth::attempt($credentials)) {
-//         $user = new \App\Models\User();
+    if (!Auth::attempt($credentials)) {
+        $user = new \App\Models\User();
 
-//         $user->name = 'Admin';
-//         $user->email = $credentials['email'];
-//         $user->password = Hash::make($credentials['password']);
+        $user->name = 'Admin';
+        $user->email = $credentials['email'];
+        $user->password = Hash::make($credentials['password']);
 
-//         $user->save();
+        $user->save();
 
-//         if (Auth::attempt($credentials)) {
-//             $user = Auth::user();
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
 
-//             $adminToken = $user->createToken('admin-token');
-//         }
-//     }
-// });
+            $adminToken = $user->createToken('admin-token', ['create', 'update', 'delete']);
+            $updateToken = $user->createToken('update-token', ['create', 'update']);
+            $basicToken = $user->createToken('basic-token');
+
+            return [
+                'admin' => $adminToken->plainTextToken,
+                'update' => $updateToken->plainTextToken,
+                'basic' => $basicToken->plainTextToken,
+            ];
+        }
+    }
+});
